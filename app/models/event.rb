cacdd3c053c7380800723501208530e0ac4ac0dd
Jie Hu Zhao <jiehuzhao@hotmail.com>
2020-12-08 13:05:46 +0100

    added pg search and index method

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,11 @@
 class Event < ApplicationRecord
   belongs_to :user
   belongs_to :organization
+
+  include PgSearch::Model
+  pg_search_scope :search_by_location,
+    against: [:location],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
