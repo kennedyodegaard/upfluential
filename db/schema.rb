299cdfa21cc0ad046e9c3c067d67ffb5b82633e9
Jie Hu Zhao <jiehuzhao@hotmail.com>
2020-12-08 14:50:29 +0100

    not seeding, made some changes to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_08_111641) do
+ActiveRecord::Schema.define(version: 2020_12_08_122636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,8 +53,6 @@ ActiveRecord::Schema.define(version: 2020_12_08_111641) do
     t.text "description"
     t.string "location"
     t.string "category"
-    t.date "start_date"
-    t.date "end_date"
     t.datetime "start_time"
     t.datetime "end_time"
     t.integer "capacity"
