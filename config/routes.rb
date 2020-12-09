d5fc56b0026cb7640faea6cdd0f8929a8116a0fd
sefi cohen <seficohen1@gmail.com>
2020-12-09 11:59:08 +0100

    created routes and controller for bookings

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :events
+  resources :events do 
+    resources :bookings, only: [:new, :create, :destroy]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
