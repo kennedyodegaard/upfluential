71538c61427935606f895b19d6210bc25597f027
sefi cohen <seficohen1@gmail.com>
2020-12-15 12:51:54 +0100

    slider

---
+++ app/javascript/components/glide.js
@@ -0,0 +1,20 @@
+import Glide, { Arrows } from '@glidejs/glide'
+
+const initGlide = () => {
+    console.log('consolggggggghhhh')
+    new Glide('.glide', {
+        type: 'carousel',
+        animationDuration: 1000,
+        autoplay: 3000,
+        startAt: 0,
+        perView: 1,
+         // set a value to show the previous and next slides peeking in
+        peek: {
+          before: 0,
+          after: 50
+        },
+        gap: 10
+    }).mount({ Arrows })
+}
+
+export { initGlide }
