f16ad1b85877df83192976f1a5bbcd1a63f54cd1
Louis Duquesne <louisdqn@gmail.com>
2020-12-15 17:20:05 +0100

    event card opening with avatars

--- app/javascript/components/event_status.js
+++ app/javascript/components/event_status.js
@@ -3,6 +3,8 @@ const upcomingButton = document.querySelector(".dashboard-cards.upcoming");
 const completedButton = document.querySelector(".dashboard-cards.completed");
 const  upcomingEvents = document.querySelector(".upcoming-events");
 const  completedEvents = document.querySelector(".completed-events");
+const myEventsButton = document.querySelector(".dashboard-cards-my-event")
+const myEvents = document.querySelector(".my-events")
 
 const checkActivityStatus = () => {
     if (upcomingEvents) {
@@ -11,6 +13,8 @@ const checkActivityStatus = () => {
         completedEvents.classList.add("d-none");
         completedButton.classList.add("disable")
         upcomingButton.classList.remove("disable")
+        myEvents.classList.add("d-none");
+        myEventsButton.classList.add("disable");
       });
 
      completedButton.addEventListener('click', (event) =>  {
@@ -18,7 +22,18 @@ const checkActivityStatus = () => {
         upcomingEvents.classList.add("d-none");
         upcomingButton.classList.add("disable")
         completedButton.classList.remove("disable")
+        myEvents.classList.add("d-none");
+        myEventsButton.classList.add("disable");
     });
+
+     myEventsButton.addEventListener('click', (event) => {
+      myEvents.classList.remove("d-none");
+      myEventsButton.classList.remove("disable");
+      completedButton.classList.add("disable");
+      completedEvents.classList.add("d-none");
+      upcomingEvents.classList.add("d-none");
+      upcomingButton.classList.add("disable")
+     });
     }
   }
 
---
+++ app/javascript/components/show_attendees.js
@@ -0,0 +1,17 @@
+const arrowsDown = document.querySelectorAll(".arrow-down-attendees");
+const attendeesBox = document.querySelector(".event-attendees");
+const eventCard = document.querySelector(".event-card-attendees");
+
+const showAttendees = () => {
+  if (attendeesBox) {
+    arrowsDown.forEach((arrow) => {
+      arrow.addEventListener('click', (event) => {
+        event.currentTarget.nextElementSibling.classList.toggle('d-none');
+        event.currentTarget.parentNode.classList.toggle('open');
+      });
+    });
+  }
+}
+
+
+export { showAttendees };
